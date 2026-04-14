59be6b72de2014536b8ad6ab80da58276cdd0a12
Sullivan071028 <Your Email 3697865302@qq.com>
2026-04-14 09:27:04 +0800

    Sullivan add a view_items()

---
+++ Main code/view_items.py
@@ -0,0 +1,25 @@
+def view_items():
+    try:
+        with open("items.txt", "r") as f:
+            lines = f.readlines()
+    except:
+        print("The txt doesn't exit.")
+        return
+
+    if len(lines) == 0:
+        print("There's no content in the txt.")
+        return
+
+    print("\nInventory List:")
+
+    count = 1
+    for line in lines:
+        line = line.strip()
+
+        if line != "":
+            parts = line.split(",")
+            name = parts[0]
+            quantity = parts[1]
+
+            print(count, "-","Name : " ,name, "| Quantity : ", quantity)
+            count += 1
