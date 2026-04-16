3e8af9cbf7cf35f623b1f7eff51c23b7a4a75161
Jay-Xiang <2024077135GYE@cic.care>
2026-04-16 13:13:06 -0400

    Jay add testing file and add an new line in my function

---
+++ test_code/Jay_test_file.py
@@ -0,0 +1,24 @@
+#Automated testing
+
+import unittest
+
+def save_item(item_name:str, item_quantity:int):
+    try:
+        with open("items.txt", "a") as file:
+            file.write(f"{item_name},{item_quantity}\n")
+        print(f"\nSaved: {item_name} | Quantity: {item_quantity}")
+        return "SUCCESS" # For testing purposes only
+    except FileNotFoundError:
+        print("Failed to save item. Error: items.txt/inventory does not exist")
+
+class TestYourFunction(unittest.TestCase):
+
+    def test_save_valid_item(self):
+        # Test normal case
+        result = save_item("Apple", 10)
+        self.assertEqual(result, "SUCCESS")
+    
+    #No test needed for error case
+
+if __name__ == '__main__':
+    unittest.main()
