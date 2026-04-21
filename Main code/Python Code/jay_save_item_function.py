ecb606c7b2d2a9aade5bf5ffed3513745a1c7f30
Jay-Xiang <2024077135GYE@cic.care>
2026-04-20 21:58:59 -0400

    Jay delete unnecessary document

--- Main code/Python Code/jay_save_item_function.py
+++
@@ -1,9 +0,0 @@
-def save_item(item_name:str, item_quantity:int):
-    try:
-        with open("items.txt", "a") as file:
-            file.write(f"{item_name},{item_quantity}\n")
-        print(f"\nSaved: {item_name} | Quantity: {item_quantity}")
-        return "SUCCESS" # For testing purposes only
-    except FileNotFoundError:
-        print("Failed to save item. Error: items.txt/inventory does not exist")
-        return "FAILURE" # For testing purposes only
